8d517c64d18a2d38fd1595342b0e07a37ef7bb6b
Jerome <nietsemorej@gmail.com>
2024-11-05 13:45:45 -0800

    localize page metadata

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,21 +1,30 @@
 // app/[locale]/layout.tsx
 
 import Header from "@/app/_components/Header";
-import type { Metadata } from "next";
 import "@/app/globals.css";
 import { locales } from "@/i18n.config";
-import { unstable_setRequestLocale } from "next-intl/server";
+import { getTranslations, unstable_setRequestLocale } from "next-intl/server";
 import useTextDirection from "@/app/_hooks/useTextDirection";
 
 export function generateStaticParams() {
   return locales.map((locale) => ({ locale }));
 }
 
-export const metadata: Metadata = {
-  title: "Next.js Weather",
-  description:
-    "A weather app built with Next.js and next-intl",
-};
+export async function generateMetadata({
+  params: { locale },
+}: {
+  params: { locale: string };
+}) {
+  const t = await getTranslations({
+    locale,
+    namespace: "Layout.metaData",
+  });
+
+  return {
+    title: t("title"),
+    description: t("description"),
+  };
+}
 
 export default function LocaleLayout({
   children,
