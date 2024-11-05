e612c161291b43b086eff392445e074bb0893d0a
Jerome <nietsemorej@gmail.com>
2024-11-05 11:09:47 -0800

    ensure static rendering (?? cannot run build to check the output, should try later )

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,8 +1,14 @@
-// app/layout.tsx
+// app/[locale]/layout.tsx
 
 import Header from "@/app/_components/Header";
 import type { Metadata } from "next";
 import "@/app/globals.css";
+import { locales } from "@/i18n.config";
+import { unstable_setRequestLocale } from "next-intl/server";
+
+export function generateStaticParams() {
+  return locales.map((locale) => ({ locale }));
+}
 
 export const metadata: Metadata = {
   title: "Next.js Weather",
@@ -21,6 +27,7 @@ export default function LocaleLayout({
     locale: string;
   };
 }>) {
+  unstable_setRequestLocale(locale);
   return (
     <html lang={locale}>
       <body className="mx-auto w-[500px] bg-slate-950 px-6 pt-3 text-sky-100">
--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,5 +1,26 @@
 import ServerWeatherAlerts from "@/app/_components/WeatherAlerts/ServerWeatherAlerts";
-export default function Home() {
+import { unstable_setRequestLocale } from "next-intl/server";
+
+export default function Home({
+  params: { locale },
+}: Readonly<{ params: { locale: string } }>) {
+  /*
+   * Some notes on static rendering
+   * - The unstable_setRequestLocale is a temporary solution 
+   * due to Next.js limitations: 
+   * Server Components can’t directly access route parameters like locale. 
+   * Future next-intl updates may eliminate this requirement. 
+   * Find out more in the next-intl Static rendering documentation.
+   * - Using generateStaticParams to specify every supported locale 
+   * can impact build performance, especially for sites with many locales. 
+   * It’s often more efficient to specify only the default locale 
+   * and dynamically generate others as needed.
+   * - Statically exporting your site with next-intl has 
+   * some important limitations. Refer to the Usage 
+   * without middleware (static export) section 
+   * in the next-intl documentation for guidance.
+   */
+  unstable_setRequestLocale(locale);
   return (
     <main>
       <h1 className="text-xs font-thin">
